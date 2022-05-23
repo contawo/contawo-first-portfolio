4ec4659eb0521cd3c128da24defb747b0d64b032
Awonke Mnotoza <mnotozawonke@gmail.com>
2022-05-24 00:14:14 +0200

    Form Submission
    Updated the form so that it works.
    Did some few animations with the navigation's

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,45 @@
-import React from 'react'
-import { NavLink } from "react-router-dom"
+import React, {useRef, useEffect} from 'react';
+import { NavLink } from "react-router-dom";
+import { gsap } from "gsap";
 
 const Navigation = () => {
+  //~ Getting Element from the DOM
+  let home = useRef(null)
+  let about = useRef(null)
+  let project = useRef(null)
+  let contact = useRef(null)
+
+  //* Animation with gsap
+  useEffect(() => {
+    //^ From
+    //^ To
+    gsap.from(home, { rotation: "+=360" });
+    gsap.from(about, { rotation: "+=360" });
+    gsap.from(project, { rotation: "+=360" });
+    gsap.from(contact, { rotation: "+=360" });
+  }, [])
+
   return (
     <div className="nav">
-      <NavLink to="/" className="nav-link">
-        <i class="fa-solid fa-house"></i>
+      <NavLink 
+        to="/" className="nav-link"
+        ref={el => home = el}>
+          <i class="fa-solid fa-house"></i>
       </NavLink>
-      <NavLink to="/about" className="nav-link">
-        <i class="fa-solid fa-face-grin"></i>
+      <NavLink 
+        to="/about" className="nav-link"
+        ref={el => about = el}>
+          <i class="fa-solid fa-face-grin"></i>
       </NavLink>
-      <NavLink to='/projects' className="nav-link">
-        <i class="fa-solid fa-folder-plus"></i>
+      <NavLink 
+        to='/projects' className="nav-link"
+        ref={el => project = el}>
+          <i class="fa-solid fa-folder-plus"></i>
       </NavLink>
-      <NavLink to="/contact" className="nav-link">
-        <i class="fa-solid fa-phone"></i>
+      <NavLink 
+        to="/contact" className="nav-link"
+        ref={el => contact = el}>
+          <i class="fa-solid fa-phone"></i>
       </NavLink>
       <i class="fa-solid fa-bars nav-button"></i>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navigation from '../components/Navigation';
+import { send } from "emailjs-com"
 
 const Contact = () => {
+    const [userName, setUsername] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+
+    const handleName = (e) => {
+        setUsername(e.target.value)
+    }
+    const handleMessage = (e) => {
+        setUserMessage(e.target.value)
+    }
+    const handleEmail = (e) => {
+        setUserEmail(e.target.value)
+    }
+    const submitMail = (e) => {
+        e.preventDefault();
+        send(
+            'service_xy38mbr',
+            'template_8cdydve',
+            {userName, userMessage, userEmail},
+            'DFfU6nD-YpIx8JyA7'
+        ).then(response => {
+            alert("Email recieved")
+        }).catch(err => {
+            alert("Email failed to send")
+        })
+        setUsername('');
+        setUserEmail('');
+        setUserMessage('');
+    }
+
     return (
         <div className="contact">
             <Navigation/>
@@ -10,19 +41,38 @@ const Contact = () => {
                     <i class="fa-solid fa-bars nav-button"></i>
                     <h1>Contact</h1>
                 </div>
-                <form className="contact-form">
+                <form 
+                    className="contact-form"
+                    onSubmit={submitMail}>
                     <div className="text-input">
-                        <input type="text" placeholder="Enter your name"/>
+                        <input 
+                            type="text"
+                            name="user-name"
+                            value={userName}
+                            required
+                            placeholder="Enter your name"
+                            onChange={handleName}/>
                     </div>
                     <div className="text-message">
-                        <textarea placeholder="Message"/>
-                        <div className="send-message">
+                        <textarea
+                            value={userMessage}
+                            required
+                            placeholder="Message"
+                            name="user-message"
+                            onChange={handleMessage}/>
+                        <button className="send-message" type="submit">
                             <h1>Send</h1>
                             <i class="fa-solid fa-paper-plane"></i>
-                        </div>
+                        </button>
                     </div>
                     <div className="text-input">
-                        <input type="email" placeholder="Enter your email"/>
+                        <input 
+                            type="email"
+                            name="user-email"
+                            required
+                            value={userEmail}
+                            onChange={handleEmail}
+                            placeholder="Enter your email"/>
                     </div>
                 </form>
                 <div className="socials">
